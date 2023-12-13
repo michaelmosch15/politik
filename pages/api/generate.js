67b63d8e6a9f9a51dd54081de1fc019f9910ca68
michaelmosch15 <alphonse52leo@gmail.com>
2023-12-12 22:35:20 -0500

    first commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,11 +15,11 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const news = req.body.news || '';
+  if (news.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid topic",
       }
     });
     return;
@@ -28,18 +28,19 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(news),
       temperature: 0.6,
+      max_tokens: 400,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch(error) { 
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
     } else {
       console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
+      res.status(200).json({
         error: {
           message: 'An error occurred during your request.',
         }
@@ -48,15 +49,13 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(news) {
+  const capitalizedNews =
+    news[0].toUpperCase() + news.slice(1).toLowerCase();
+  return `Write a news article based upon the topic. Do not include specific details. NO names. Make it 2 paragraphs.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+Topic: Biden Warns Israel It Is Losing Support Over War
+Article: President Biden told Israels leaders on Tuesday that they were losing international support for their war in Gaza, exposing a widening rift with Prime Minister Benjamin Netanyahu, who rejected out of hand the American vision for a postwar resolution to the conflict.
+Topic: ${capitalizedNews}
+Article:`;
 }
